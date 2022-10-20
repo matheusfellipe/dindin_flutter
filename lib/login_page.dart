07302dadc8b734b9de9efdd2ae262d55b9136a06
Matheus Fellipe <matheus060399@gmail.com>
2022-10-20 14:30:39 -0400

    implementing login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,25 +1,46 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget{
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage>{
+class _LoginPageState extends State<LoginPage> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:Colors.purple[300],
-      
+      backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
           child: Column(children: [
-            Text('Hello World',
-            style: TextStyle(fontWeight: FontWeight.bold,fontSize: 24),
+            SizedBox(height: 25),
+            Text(
+              'Welcome to Din Din Com',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'It is a pleasure have you here',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  border: Border.all(color: Colors.purple),
+                  ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
             ),
           ]),
         ),
