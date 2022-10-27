ee94d3b4c0e29c0ddb699a387b3bf10e8a60a354
Matheus Fellipe <matheus060399@gmail.com>
2022-10-27 12:48:36 -0400

    improving login screen

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -16,26 +17,30 @@ class _LoginPageState extends State<LoginPage> {
       backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
-          child: Column(children: [
-            SizedBox(height: 25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+           
             Text(
               'Seja bem vindo ao Din Din Com',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              style: GoogleFonts.bebasNeue(
+                fontSize: 36,
+              ),
             ),
             SizedBox(height: 10),
             Text(
               'É uma alegria ser seu parceiro',
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 50),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 25.0),
               child: Container(
                 decoration: BoxDecoration(
                   color: Colors.grey[200],
-                  border: Border.all(color: Colors.purple),
+                  border: Border.all(color: Colors.white),
                   borderRadius: BorderRadius.circular(12),
-                  ),
+                ),
                 child: Padding(
                   padding: const EdgeInsets.only(left: 20.0),
                   child: TextField(
@@ -47,7 +52,7 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ),
             ),
-             SizedBox(height: 20),
+            SizedBox(height: 20),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 25.0),
               child: Container(
@@ -55,8 +60,7 @@ class _LoginPageState extends State<LoginPage> {
                   color: Colors.grey[200],
                   border: Border.all(color: Colors.white),
                   borderRadius: BorderRadius.circular(12),
-                  ),
-
+                ),
                 child: Padding(
                   padding: const EdgeInsets.only(left: 20.0),
                   child: TextField(
@@ -69,23 +73,46 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ),
             ),
-
             SizedBox(height: 10),
-
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal:25.0),
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
               child: Container(
                 padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(color: Colors.deepPurple,
-                borderRadius: BorderRadius.circular(12)
-                ),
-                child: Text('Entrar',
-                style: TextStyle(color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 18
-                ),
+                decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(12)),
+                child: Center(
+                  child: Text(
+                    'Entrar',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
                 ),
               ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Não está cadastrado?',
+                  style: TextStyle(
+                   
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  ' Registrar agora',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
             ),
           ]),
         ),
